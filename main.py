625a23c039bc9f22d22c9689d1ba0e71faef81f3
Z-Link-C <zachery.croft@gmail.com>
2026-05-11 04:04:13 -0400

    nearly done! again!

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 # lib/cli_tool.py
 import argparse
 import sys
-from models import Users, Projects,Tasks
-from utils import load, save, print_table
+from lib.models import Users, Projects,Tasks
+from lib.utils import load, save, print_table
 
 #---Users
 def add_user(args):
